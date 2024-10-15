21a5d3a0705cf7aa48e51aacb218f6bccc314900
Ubuntu <ubuntu@ip-172-31-7-24.ap-south-1.compute.internal>
2024-10-15 19:26:36 +0000

    modified populate_database.py to add system path

--- Backend1/services/populate_database.py
+++ Backend1/services/populate_database.py
@@ -4,6 +4,8 @@ from google_spreadsheet import populate_answers_from_spreadsheet,populate_questi
 spreadsheet_id = "1kwHVw2V32SvgUWGn398YekCjHyfk4vUF4fouTV-j5-M"
 credentials_path = "../mentormate-googlecloud.json"
 
+# Add the parent directory to the Python path
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 populate_units_from_spreadsheet(spreadsheet_id, credentials_path)
 populate_questions_from_spreadsheet(spreadsheet_id, credentials_path)
